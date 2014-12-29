af4535d6ced418828aad08a36cd0609f09aab1f1
unknown <carl.vanderwegen.mivix@gmail.com>
2014-12-29 23:52:03 +0100

    require

---
+++ solutions/opdracht-include-require-031/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+$articles           =   array(  0 => array("title" => "value", "text" => "value" , "tags" => array()),
+                                1 => array("title" => "value", "text" => "value" , "tags" => array()),
+                                2 => array("title" => "value", "text" => "value" , "tags" => array()));
+
+
+
+
+include 'view/head.html';
+include 'view/body.html';
+include 'view/footer.html';
+
+
+
+
+
+
+
+ ?>
